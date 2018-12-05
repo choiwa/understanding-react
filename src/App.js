cf617b82b99b8bd3f37628b0984a7b4ec04215aa
Joyce Cho <joyjoycho@gmail.com>
2018-12-04 19:08:59 -0800

    parcel auto add react and react-dom to package.json by saving react and react-dom to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const Pet = props => {
-  return React.createElement("div", {}, []);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, [
+      React.createElement("h1", {}, props.name),
+      React.createElement("h2", {}, props.animal),
+      React.createElement("h2", {}, props.breed)
+    ])
+  ]);
 };
+
+class App extends React.Component {
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, "Adopt me!"),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "havanese"
+      })
+    ]);
+  }
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
