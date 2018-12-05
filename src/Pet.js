ac7c3656a7b4da424671211383aefb346b7c699b
Joyce Cho <joyjoycho@gmail.com>
2018-12-04 22:44:40 -0800

    convert to jsx format

--- src/Pet.js
+++ src/Pet.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 export const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, [
-      React.createElement("h1", {}, props.name),
-      React.createElement("h2", {}, props.animal),
-      React.createElement("h2", {}, props.breed)
-    ])
-  ]);
+  // without jsx
+  //   return React.createElement("div", {}, [
+  //     React.createElement("h1", {}, props.name),
+  //     React.createElement("h2", {}, props.animal),
+  //     React.createElement("h2", {}, props.breed)
+  //   ]);
+
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.animal}</h1>
+      <h1>{props.breed}</h1>
+    </div>
+  );
 };
 
 export default Pet;
